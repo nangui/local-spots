427985a07d721ddd1471d1331223419e0b2d077c
Adonai Nangui <adonainangui03@gmail.com>
2025-09-01 03:38:28 +0000

    feat: add swagger config and routes

--- start/routes.ts
+++ start/routes.ts
@@ -1,13 +1,16 @@
-/*
-|--------------------------------------------------------------------------
-| Routes file
-|--------------------------------------------------------------------------
-|
-| The routes file is used for defining the HTTP routes.
-|
-*/
+import AutoSwagger from "adonis-autoswagger";
+import swagger from "#config/swagger";
 
 import router from '@adonisjs/core/services/router'
+
+// returns swagger in YAML
+router.get("/swagger", async () => {
+  return AutoSwagger.default.docs(router.toJSON(), swagger);
+});
+
+router.get("/docs", async () => {
+  return AutoSwagger.default.ui("/swagger", swagger);
+});
 
 router.get('/', async () => {
   return {
